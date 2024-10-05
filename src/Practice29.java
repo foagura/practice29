02ae0333fc72fe2527d9ac56e60a3749760f1636
foagura <foagura_0416@yahoo.co.jp>
2024-10-05 13:28:25 +0900

    first commit

---
+++ src/Practice29.java
@@ -0,0 +1,44 @@
+class Super {
+    int id;
+
+    Super() {
+        id = 100;
+    }
+
+    void methodA() {
+        System.out.println("Superクラスのid：" + id);
+    }
+}
+
+class Sub extends Super {
+    int id;
+
+    Sub() {
+        super();    // スーパークラスのコンストラクタを呼び出す
+    }
+
+    Sub(int id) {
+        this.id = id;
+    }
+
+    void methodA() {
+        System.out.println("superで呼び出したメソッドの戻り値が下記。");
+        super.methodA();    // スーパークラスのメソッドを呼び出す
+    }
+
+    void methodB() {
+        System.out.println("Subクラスのid：" + id);
+    }
+}
+
+public class Practice29 {
+    public static void main(String[] args) {
+        Super superObj = new Super();
+        Sub subObjA = new Sub();
+        Sub subObjB = new Sub(9999);
+
+        superObj.methodA();
+        subObjA.methodA();
+        subObjB.methodB();
+    }
+}
